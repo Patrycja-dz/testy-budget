6943c7c36daf89e562944139dc9974e3cf6742a4
Patrycja-dz <dziemianko.pat@gmail.com>
2022-06-02 19:00:50 +0200

    styling instructors section

--- src/pages/Instructors.js
+++ src/pages/Instructors.js
@@ -1,40 +1,38 @@
-import React from 'react'
-import lady from './../images/lady-in-yoga.jpg'
-import instructor1 from './../images/instructor1.jpg'
-import instructor2 from './../images/instructor2.jpg'
-import instructor3 from './../images/instructor3.jpg'
-import instructor4 from './../images/instructor4.jpg'
+import React from "react";
+import lady from "./../images/lady-in-yoga.jpg";
+import instructor1 from "./../images/instructor1.jpg";
+import instructor2 from "./../images/instructor2.jpg";
+import instructor3 from "./../images/instructor3.jpg";
+import instructor4 from "./../images/instructor4.jpg";
 function Instructors() {
   return (
     <>
-    <h2 className='heading'>Instructors</h2>
-		<div className="content">
-			<div>
-				<ul className="section">
-					<li>
-						<img src={instructor1} alt="Yoga instructor one"></img>
-						<h4>Belle Humpfree</h4>
-					</li>
-					<li>
-						<img src={instructor2} alt="Yoga instructor two"></img>
-						<h4>Jacob Collern</h4>
-					</li>
-				</ul>
-				<ul className="section">
-					<li>
-						<img src={instructor3} alt="Yoga instructor three"></img>
-						<h4>Edward Blanche</h4>
-					</li>
-					<li>
-						<img src={instructor4} alt="Yoga instructor four"></img>
-						<h4>Carrie Waldorf</h4>
-					</li>
-				</ul>
-			</div>
-            <img src={lady} alt="lady in yoga"></img>
-		</div>
-        </>
-  )
+      <h2 className="heading heading--moved">Instructors</h2>
+      <section className="section">
+        <section className="gallery">
+          <figure className="figure">
+            <img src={instructor1} alt="Yoga instructor one"></img>
+            <figcaption className="figure__caption">Belle Humpfree</figcaption>
+          </figure>
+          <figure>
+            <img src={instructor2} alt="Yoga instructor two"></img>
+            <figcaption className="figure__caption">Jacob Collern</figcaption>
+          </figure>
+          <figure className="figure">
+            <img src={instructor3} alt="Yoga instructor three"></img>
+            <figcaption className="figure__caption">Edward Blanche</figcaption>
+          </figure>
+          <figure className="figure">
+            <img src={instructor4} alt="Yoga instructor four"></img>
+            <figcaption className="figure__caption">Carrie Waldorf</figcaption>
+          </figure>
+        </section>
+        <div className="women">
+          <img src={lady} alt="lady in yoga"></img>
+        </div>
+      </section>
+    </>
+  );
 }
 
-export default Instructors
+export default Instructors;
