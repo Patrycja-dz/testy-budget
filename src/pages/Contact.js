203871f4ed979fb34741b1e7cdc0fd5d858835bd
Patrycja-dz <dziemianko.pat@gmail.com>
2022-06-02 20:34:30 +0200

    adding classes to contact form

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,26 +3,41 @@ import React from "react";
 function Contact() {
   return (
     <>
-      <h2>Contact</h2>
-      <form>
-        <h3>Inquiries</h3>
-        <label for="name">
-          <span>Name</span>
-          <input type="text" id="name" />
-        </label>
-        <label for="email">
-          <span>Email</span>
-          <input type="text" id="email" />
-        </label>
-        <label for="subject">
-          <span>Subject</span>
-          <input type="text" id="subject" />
-        </label>
-        <label for="message">
-          <span>Message</span>
-          <textarea name="message" id="message" cols="30" rows="10"></textarea>
-        </label>
-        <input type="submit" id="send" value="Send" />
+      <h2 className="heading">Contact</h2>
+      <form className="form ">
+        <fieldset className="form__fieldset">
+          <h3>Inquiries</h3>
+          <p>
+            <label for="name" className="form__label">
+              <span>Name</span>
+              <input type="text" id="name" />
+            </label>
+          </p>
+          <p>
+            <label for="email" className="form__label">
+              <span>Email</span>
+              <input type="text" id="email" />
+            </label>
+          </p>
+          <p>
+            <label for="subject" className="form__label">
+              <span>Subject</span>
+              <input type="text" id="subject" />
+            </label>
+          </p>
+          <p>
+            <label for="message" className="form__label">
+              <span>Message</span>
+              <textarea
+                name="message"
+                id="message"
+                cols="30"
+                rows="10"
+              ></textarea>
+            </label>
+          </p>
+        </fieldset>
+        <button className="form__button">Send</button>
       </form>
     </>
   );
