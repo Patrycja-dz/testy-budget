87a63f6e2b2388b74ca281b8b835c210238d377f
Patrycja-dz <dziemianko.pat@gmail.com>
2022-06-03 06:30:43 +0200

    continuation of styling the form

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,43 +3,46 @@ import React from "react";
 function Contact() {
   return (
     <>
-    <h2 className="heading">Contact</h2>
-    
-    <div className="contact-container">
-      <form className="form ">
-        <fieldset className="form__fieldset">
-          <h3 className="article__title">Inquiries</h3>
-         
-            <label for="name" className="form__label">
-              <span>Name</span>
-              <input type="text" id="name" className="form__field"/>
-            </label>
-      
-            <label for="email" className="form__label">
-              <span>Email</span>
-              <input type="text" id="email" className="form__field" />
-            </label>
-   
-        
-            <label for="subject" className="form__label">
-              <span>Subject</span>
-              <input type="text" id="subject" className="form__field" />
-            </label>
-       
-            <label for="message" className="form__label">
-              <span>Message</span>
-              <textarea
-                name="message"
-                id="message"
-                cols="30"
-                rows="10"
-                className="form__field"></textarea>
-            </label>
-        
-        </fieldset>
-        <button className="form__button">Send</button>
-      </form>
-    </div>
+      <h2 className="heading">Contact</h2>
+
+      <div className="contact-container">
+        <form className="form">
+          <fieldset className="form__fieldset">
+            <h3 className="form__title">Inquiries</h3>
+            <p>
+              <label for="name" className="form__label">
+                <span>Name</span>
+                <input type="text" id="name" className="form__field" />
+              </label>
+            </p>
+            <p>
+              <label for="email" className="form__label">
+                <span>Email</span>
+                <input type="text" id="email" className="form__field" />
+              </label>
+            </p>
+            <p>
+              <label for="subject" className="form__label">
+                <span>Subject</span>
+                <input type="text" id="subject" className="form__field" />
+              </label>
+            </p>
+            <p>
+              <label for="message" className="form__label">
+                <span>Message</span>
+                <textarea
+                  name="message"
+                  id="message"
+                  cols="30"
+                  rows="10"
+                  className="form__field textarea"
+                ></textarea>
+              </label>
+            </p>
+          </fieldset>
+          <button className="form__button">Send</button>
+        </form>
+      </div>
     </>
   );
 }
