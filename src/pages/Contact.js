5abbe27259eaace4a7d3c5c07ce3e85176aa833b
Patrycja-dz <dziemianko.pat@gmail.com>
2022-06-09 16:03:43 +0200

    disabled subbmit  button before fill correct field

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -6,7 +6,7 @@ import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 function Contact() {
-  let navigate = useNavigate();
+  const navigate = useNavigate();
 
   let schema = yup
     .object({
@@ -19,7 +19,7 @@ function Contact() {
 
   const {
     register,
-    handleSubmit,
+   handleSubmit,
     formState: { errors },
   } = useForm({
     resolver: yupResolver(schema),
@@ -29,111 +29,124 @@ function Contact() {
   const [email, setEmail] = useState("");
   const [subject, setSubject] = useState("");
   const [message, setMessage] = useState("");
-  const [isError, setIsError] = useState(false);
-  const [data, setData] = useState(null);
-  const submitData = () => {
-    setIsError(false)
-    const data = {
-      name: name,
-      email: email,
-      subject: subject,
-      message: message,
-    };
-    axios
-      .post("http://httpbin.org/post", data)
-      .then((res) => {
-        setData(res.data);
-        setName("");
-        setEmail("");
-        setSubject("");
-        setMessage("");
-      //console.log(res.data);
-       
-      })
-      .catch((err) => {
-        setIsError(true);
-      });
-    
-  };
+  const [isError, setIsError] = useState("");
+  const [success, setSuccess] = useState(false);
 
+
+  const submit = async () => {
+   // e.preventDefault();
+    try {
+      const response = await axios.post(
+        "https://httpbin.org/post",
+        JSON.stringify({ name, email, subject, message }),
+        {
+          headers: { "Context-Type": "application/json" },
+          withCredentials: true,
+        }
+      );   
+      //console.log(response.data);
+      //console.log(JSON.stringify(response));
+      setSuccess(true);
+    } catch (err) {
+      if (!err.response) {
+        setIsError("No server response");
+      } else {
+        setIsError("Send message failed");
+      }
+    }
+  };
   return (
     <>
-      <h2 className="heading">Contact</h2>
-
-      <div className="contact-container">
-        <form
-          className="form"
-          onSubmit={handleSubmit((data) => {
-            console.log(data);
-          })}
-        >
-          <fieldset className="form__fieldset">
-            <h3 className="form__title">Inquiries</h3>
-            <p>
-              <label for="name" className="form__label">
-                <span>Name</span>
-                <input
-                  type="text"
-                  id="name"
-                  className="form__field"
-                  {...register("user")}
-                  onChange={e=> setName(e.target.value)}
-                />
-
-                <p>{errors.user?.message}</p>
-              </label>
-            </p>
-            <p>
-              <label for="email" className="form__label">
-                <span>Email</span>
-                <input
-                  type="text"
-                  id="email"
-                  className="form__field"
-                  {...register("email")}
-                  onChange={e=> setEmail(e.target.value)}
-                />
-                <p>{errors.email?.message}</p>
-              </label>
-            </p>
-            <p>
-              <label for="subject" className="form__label">
-                <span>Subject</span>
-                <input
-                  type="text"
-                  id="subject"
-                  className="form__field"
-                  {...register("subject")}
-                  onChange={e=> setSubject(e.target.value)}
-                />
-              </label>
-            </p>
-            <p>
-              <label for="message" className="form__label">
-                <span>Message</span>
-                <textarea
-                  name="message"
-                  id="message"
-                  cols="30"
-                  rows="10"
-                  className="form__field textarea"
-                  {...register("message")}
-                  onChange={e=> setMessage(e.target.value)}
-                ></textarea>
-              </label>
-            </p>
-          </fieldset>
+      {success ? (
+        <section>
+          <h1>Successs</h1>
           <button
-             onClick={submitData}
-          // className="form__button"
-          // onClick={() => {
-          //   navigate("/");
-          // }}
+            onClick={() => {
+              navigate("/");
+            }}
           >
-            Send
+            Return to Home
           </button>
-        </form>
-      </div>
+        </section>
+      ) : (
+        <section>
+          <h2 className="heading">Contact</h2>
+
+          <div className="contact-container">
+            <form className="form" onSubmit={handleSubmit(submit)}>
+              <fieldset className="form__fieldset">
+                <h3 className="form__title">Inquiries</h3>
+                <p>
+                  <label htmlFor="name" className="form__label">
+                    <span>Name</span>
+                    <input
+                      type="text"
+                      id="name"
+                      className="form__field"
+                      {...register("user")}
+                      onChange={(e) => setName(e.target.value)}
+                      required
+                      autoComplete="off"
+                    />
+
+                    <p>{errors.user?.message}</p>
+                  </label>
+                </p>
+                <p>
+                  <label htmlFor="email" className="form__label">
+                    <span>Email</span>
+                    <input
+                      type="text"
+                      id="email"
+                      className="form__field"
+                      {...register("email")}
+                      onChange={(e) => setEmail(e.target.value)}
+                      required
+                      autoComplete="off"
+                    />
+                    <p>{errors.email?.message}</p>
+                  </label>
+                </p>
+                <p>
+                  <label htmlFor="subject" className="form__label">
+                    <span>Subject</span>
+                    <input
+                      type="text"
+                      id="subject"
+                      className="form__field"
+                      {...register("subject")}
+                      onChange={(e) => setSubject(e.target.value)}
+                      autoComplete="off"
+
+                    />
+                  </label>
+                </p>
+                <p>
+                  <label htmlFor="message" className="form__label">
+                    <span>Message</span>
+                    <textarea
+                      name="message"
+                      id="message"
+                      cols="30"
+                      rows="10"
+                      className="form__field textarea"
+                      {...register("message")}
+                      onChange={(e) => setMessage(e.target.value)}
+                    ></textarea>
+                  </label>
+                </p>
+              </fieldset>
+              <button id="contact-btn"
+                disabled={
+                  !name || !email|| !subject || !message ? true : false 
+                }
+              >
+                Send
+              </button>
+            </form>
+          </div>
+        </section>
+      )}
     </>
   );
 }
